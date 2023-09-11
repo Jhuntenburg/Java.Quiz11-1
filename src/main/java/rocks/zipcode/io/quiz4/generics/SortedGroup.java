b4acf9bbc027a013c25c5651f7f9ca1b5d460c2a
Joe <Jhuntenburg@gmail.com>
2023-09-11 14:56:18 -0400

    sorted group sorted

--- src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
@@ -1,18 +1,38 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class SortedGroup<T> extends Group<T> {
+
     @Override
     public void insert(T value) {
+        group.add(value);
+        Set<T> set = new TreeSet<>(group);
+        group.clear();
+        group.addAll(set);
+
+
+
+
     }
 
     @Override
     public void delete(T value) {
+        Set<T> set = new TreeSet<>(group);
+        group.clear();
+        group.addAll(set);
+        group.remove(value);
+
+
     }
 
     public Integer indexOf(T value) {
-        return null;
+
+        return group.indexOf(value);
     }
 }
