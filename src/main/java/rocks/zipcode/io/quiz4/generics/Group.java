cde9bec73522f8d8b2717c993668031e29c42612
Joe <Jhuntenburg@gmail.com>
2023-09-11 14:22:42 -0400

    group tests pass

--- src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
@@ -1,37 +1,59 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author leon on 18/12/2018.
  */
-public class Group<_> {
+public class Group<T> implements GroupInterface<T>  {
+    @Override
+    public String toString() {
+        return ""+
+                 group ;
+    }
+
+    List<T> group;
     public Group() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        this.group = new ArrayList<>();
     }
 
     public Integer count() {
-        return null;
+
+        return Math.toIntExact(this.group.stream().count());
     }
 
-    public void insert(_ value) {
+
+
+    public void insert(T value) {
+        this.group.add(value);
     }
 
-    public Boolean has(_ value) {
-        return null;
+    public Boolean has(T value) {
+
+        return this.group.contains(value);
     }
 
-    public _ fetch(int indexOfValue) {
-        return null;
+    public T fetch(int indexOfValue) {
+
+        return this.group.get(indexOfValue);
     }
 
-    public void delete(_ value) {
+
+
+
+
+    public void delete(T value) {
+        this.group.remove(value);
     }
 
     public void clear() {
+        this.group.clear();
     }
 
-    public Iterator<_> iterator() {
-        return null;
+    public Iterator<T> iterator() {
+
+        return group.iterator();
     }
 }
